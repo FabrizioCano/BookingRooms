cb2dc1846fc87b54bb8490236e284e60cd7ba56c
FabrizioCano <fabriziocano7@gmail.com>
2025-04-27 12:44:09 -0300

    fixes deploy

--- app/actions/createUser.js
+++ app/actions/createUser.js
@@ -1,6 +1,6 @@
 'use server';
 import {createAdminClient} from "@/config/appwrite";
-import { ID } from "node-appwrite";
+import { ID,Query } from "node-appwrite";
 
 async function createUser(previousState,formData) {
     const name=formData.get('name');
@@ -28,41 +28,41 @@ async function createUser(previousState,formData) {
     }
 
     //instancia de la cuenta
-    const {account} = await createAdminClient();
+    const {account,databases} = await createAdminClient();
     try {
-        //crear el usuario
-        const user=await account.create(ID.unique(),email,password,name);
-        //asignar el rol de usuario al nuevo usuario
-        try {
-            const rol_usuario = await databases.listDocuments(
-              process.env.NEXT_PUBLIC_APPWRITE_DATABASE,
-              process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ROLES,
-              [Query.equal('name', 'user')]
-            );
-            const rolId = rol_usuario.documents[0].$id;
-            await databases.createDocument(
-              process.env.NEXT_PUBLIC_APPWRITE_DATABASE,
-              process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_USERS,
-              ID.unique(),
-              {
-                userId: user.$id,
-                roleId: rolId,
-              }
-            );
-        } catch (err) {
-            console.error("Error assigning role to user:", err);
-            throw err;
+        // Crear usuario
+        const user = await account.create(ID.unique(), email, password, name);
+
+        // Buscar rol 'user'
+        const roles = await databases.listDocuments(
+            process.env.NEXT_PUBLIC_APPWRITE_DATABASE,
+            process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ROLES,
+            [Query.equal('name', ['user'])] // IMPORTANTE: el valor es un array
+        );
+
+        if (roles.documents.length === 0) {
+            return { error: 'Role "user" not found' };
         }
-        return {
-            success:true,
-        }
+
+        const roleId = roles.documents[0].$id;
+
+       
+        await databases.createDocument(
+            process.env.NEXT_PUBLIC_APPWRITE_DATABASE,
+            process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_USER_ROLES,
+            ID.unique(),
+            {
+                user_id: user.$id,
+                roles_id: [roleId],
+            }
+        );
+
+        return { success: true };
 
     } catch (error) {
-        return {
-            error:'Error in the registration',
-        };
+        console.error("Error creating user:", error);
+        return { error: 'An error occurred during registration' };
     }
-
 }
 
 export default createUser;
